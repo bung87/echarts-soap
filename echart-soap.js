58a10a6e66db6f581df6051c347e8e80e5bb5a88
zhoupeng <zhoupeng@xjgreat.com>
2017-10-21 00:39:12 +0800

    clean

--- echart-soap.js
+++ echart-soap.js
@@ -1,121 +1,118 @@
 (function(global, echarts) {
+
     var util = echarts.util;
+
     global.echartsSoap = echartsSoap = {};
+
     var preprocessors = {
-        "bar":[],
-        "pie":[]
-    },
-    preprocessorsMap = {
-        "barMinHeight":{
-            chartType:"bar",
-            handle:function(value){
-                return function(option){
-                    this.barMinHeight = value;
-                    console.log("handle",this)
-                }
-            }
+
+            "bar": [],
+            "pie": []
         },
-        "innerPieLabelMinPercent":{
-            chartType:"pie",
-            handle:function(value){
-                return function(option){
+        preprocessorsMap = {
 
-                    if (this.label.normal.position = "inner" || this.label.normal.position == "inside") {
-
-                        amount = echarts.util.reduce(this.data, function(sum, item) {
-                            return sum + item.value;
-                        }, 0)
-                        console.log("amount:",amount);
-                        echarts.util.each(this.data, function(item) {
-                            console.log(percent = item.value / amount)
-                            // safeNestedObjectInspection(item, "label.normal.show", item.value / amount > parseFloat(value) / 100);
-                            item.label ={
-                                normal:{
-                                    show:item.value / amount > parseFloat(value) / 100
-                                }
-                            } 
-                        });
-                        console.log("pie", this)
+            "barMinHeight": {
+                chartType: "bar",
+                handle: function(value) {
+                    return function(option) {
+                        this.barMinHeight = value;
                     }
-
                 }
-            }
-        },
-        "barShadow":{ //官方示例会将阴影数据显示在tooltip里 http://echarts.baidu.com/demo.html#bar-gradient
-            chartType:"bar",
-            handle:function(value){
-                return function(option){
+            },
+            "innerPieLabelMinPercent": {
+                chartType: "pie",
+                handle: function(value) {
+                    return function(option) {
 
-                    if(typeof this.barWidth !== "undefined"){
-                        console.warn("nothing"); //http://echarts.baidu.com/option.html#series-bar.barWidth
-                    }
-                    var dataMax = this.data.sort(function(a,b){
-                        if(util.isObject(a)){
-                            return a.value - b.value;
-                        }else{
-                            return a - b;
+                        if (this.label.normal.position = "inner" || this.label.normal.position == "inside") {
+
+                            amount = echarts.util.reduce(this.data, function(sum, item) {
+                                return sum + item.value;
+                            }, 0)
+
+                            echarts.util.each(this.data, function(item) {
+
+                                safeNestedObjectInspection(item, "label.normal.show", item.value / amount > parseFloat(value) / 100);
+
+                            });
                         }
 
-                    })[this.data.length - 1] ;
-                    var dataShadow = [];
-                    for (var i = this.data.length - 1; i >= 0; i--) {
-                        dataShadow.push(dataMax);
                     }
-                    console.log("option",option)
-                    option.series.unshift(
-                        { // For shadow
+                }
+            },
+            "barShadow": { //官方示例会将阴影数据显示在tooltip里 http://echarts.baidu.com/demo.html#bar-gradient
+                chartType: "bar",
+                handle: function(value) {
+                    return function(option) {
+
+                        var dataMax = this.data.sort(function(a, b) {
+                            if (util.isObject(a)) {
+                                return a.value - b.value;
+                            } else {
+                                return a - b;
+                            }
+
+                        })[this.data.length - 1];
+                        var dataShadow = [];
+                        for (var i = this.data.length - 1; i >= 0; i--) {
+                            dataShadow.push(dataMax);
+                        }
+                        option.series.unshift({ // For shadow
                             type: 'bar',
-                            silent:true,
+                            silent: true,
                             itemStyle: {
-                                normal: {color: value}
+                                normal: {
+                                    color: value
+                                }
                             },
-                            barWidth:this.barWidth,
-                            barGap:'-100%',
-                            barCategoryGap:'50%',
+                            barWidth: this.barWidth,
+                            barGap: '-100%',
+                            barCategoryGap: '50%',
                             data: dataShadow,
                             animation: false,
-                            label:{
-                                normal:{
-                                    show:false
+                            label: {
+                                normal: {
+                                    show: false
                                 }
                             },
-                            legendHoverLink:false,
-                            tooltip:{
-                                show:false
+                            legendHoverLink: false,
+                            tooltip: {
+                                show: false
                             }
-                        }
-                    )
+                        })
+                    }
                 }
             }
-        }
-    };
+        };
     echarts.registerPreprocessor(function(option) {
-            echarts.util.each(option.series, function(seriesOne) {
-                switch (seriesOne.type) {
-                    case "bar":
-                        echarts.util.each(preprocessors.bar,function(handle){
-                            handle().call(seriesOne,option);
-                        });
-                        break;
-                    case "pie":
-                        echarts.util.each(preprocessors.pie,function(handle){
-                            handle().call(seriesOne,option);
-                        });
-                       
 
-                        break;
+        echarts.util.each(option.series, function(seriesOne) {
 
-                }
-            })
-     });
-    function _seriesAttr(key,value){
-        this[key] = value;
-    }
-    function _registerPreprocessor(key,value){
+            switch (seriesOne.type) {
+                case "bar":
+                    echarts.util.each(preprocessors.bar, function(handle) {
+                        handle().call(seriesOne, option);
+                    });
+                    break;
+                case "pie":
+                    echarts.util.each(preprocessors.pie, function(handle) {
+                        handle().call(seriesOne, option);
+                    });
+
+
+                    break;
+
+            }
+        })
+    });
+
+    function _registerPreprocessor(key, value) {
+
         var preprocessor = preprocessorsMap[key];
-        preprocessors[preprocessor.chartType].push(preprocessor.handle.bind(null,value));
+        preprocessors[preprocessor.chartType].push(preprocessor.handle.bind(null, value));
     }
     util.extend(echartsSoap, {
+
         render: function(id, option) { //避免初始化多次 http://echarts.baidu.com/api.html#echarts.init 创建一个 ECharts 实例，返回 echartsInstance，不能在单个容器上初始化多个 ECharts 实例。
             var dom = document.getElementById(id),
                 instance = echarts.getInstanceByDom(dom),
@@ -132,52 +129,41 @@
                 instance = init(dom); //echarts "3.3.2" 地图instance.setOption(option, true);后指向地图某个区域左侧Visualmap还是初始时的这个区域的值，这里销毁instance重新init.
                 instance.setOption(option);
             }
-        },registerPreprocessor:function(key,value){
-            _registerPreprocessor(key,value);
+        },
+        registerPreprocessor: function(key, value) {
+            _registerPreprocessor(key, value);
         }
     })
 
     function safeNestedObjectInspection(obj, path, value) {
-            //arr title.textStyle.color
-            var arr = path.split("."),
-                len = arr.length,
-                last = len - 1;
-            if (len == 1 && (typeof value == "string" || typeof value == "number")) {
-                var key = arr[0];
-                obj[key] = value;
-            } else {
-                var currPath = [],
-                    curr;
-                for (var index = 0; index < len; index++) {
-                    var key = arr[index];
-                    currPath.push("['" + key + "']");
-                    curr = obj[key];
 
-                    if (index < last) {
-                        if (typeof curr === "undefined") {
-                            // curr = {};
-                            // with(obj){
+        var arr = path.split("."),
+            len = arr.length,
+            last = len - 1;
+        if (len == 1 && (typeof value == "string" || typeof value == "number")) {
+            var key = arr[0];
+            obj[key] = value;
+        } else {
+            var currPath = [],
+                curr;
+            for (var index = 0; index < len; index++) {
+                var key = arr[index];
+                currPath.push("['" + key + "']");
+                curr = obj[key];
 
-                            eval("obj" + currPath.join("") + "={};");
-                            // }
-                        }
-                    } else {
-                        // with(obj){
-                        if (typeof value == "string") {
-                            eval("obj" + currPath.join("") + "='" + value + "';");
-                        } else {
-                            eval("obj" + currPath.join("") + "=" + value + ";");
-                        }
-
-                        // }
+                if (index < last) {
+                    if (typeof curr === "undefined") {
+                        eval("obj" + currPath.join("") + "={};");
                     }
-
-
+                } else {
+                    if (typeof value == "string") {
+                        eval("obj" + currPath.join("") + "='" + value + "';");
+                    } else {
+                        eval("obj" + currPath.join("") + "=" + value + ";");
+                    }
                 }
-
             }
-
-            // console.log("safeNestedObjectInspection obj:", obj);
+        }
     }
 
 })(window, echarts);
