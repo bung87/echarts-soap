1cc6d7009816390e9469007d50cab57e083f6a51
zhoupeng <zhoupeng@xjgreat.com>
2017-10-23 12:52:09 +0800

    dist 1.0.0

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -2,51 +2,54 @@ const path = require('path');
 const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const vendorsPath = path.resolve('./node_modules');
-const env = require('yargs').argv.mode;
-const    isProduction = env === 'prod' || env === 'prodUMD';
+
 var plugins = [];
 
-if (isProduction) {
-    plugins.push(new UglifyJsPlugin({ minimize: false }));
-    // outputFile = projectName + '.min.js';
-}
 
-module.exports = {
-    entry: {
-        echartsSoap: './src/echarts-soap.js'
-    },
-    devtool: 'source-map',
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: isProduction ? 'echarts-soap.min.js' : 'echarts-soap.js',
-        library: ['echartsSoap'],
-        libraryTarget: 'umd'
-    },
-    externals: {
-        echarts: 'echarts'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env'],
-                    plugins: [require('babel-plugin-transform-object-rest-spread')]
+module.exports =  env => {
+    if (env.min) {
+        plugins.push(new UglifyJsPlugin({ minimize: false }));
+        // outputFile = projectName + '.min.js';
+    }
+    return {
+        entry: {
+            echartsSoap: './src/echarts-soap.js'
+        },
+        devtool: 'source-map',
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: env.min ? 'echarts-soap.min.js' : 'echarts-soap.js',
+            library: ['echartsSoap'],
+            libraryTarget: 'umd',
+            sourceMapFilename:'echarts-soap.min.js.map'
+        },
+        externals: {
+            echarts: 'echarts'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: [require('babel-plugin-transform-object-rest-spread')]
+                    }
                 }
-            }
-        }],
-       noParse: [
-                new RegExp(vendorsPath + '/echarts/echarts.js')
-        ]
-    },
-    resolve: {
-            alias: {
-                echarts: vendorsPath + '/echarts'
-            }
-    },
-    plugins: plugins
+            }],
+        noParse: [
+                    new RegExp(vendorsPath + '/echarts/echarts.js')
+            ]
+        },
+        resolve: {
+                alias: {
+                    echarts: vendorsPath + '/echarts'
+                }
+        },
+        plugins: plugins
+    }
 
 };
